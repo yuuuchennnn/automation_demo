da562bc5c62c66c4d4479e3335cf44b7d803ccdf
yuchen.wang <yuchen.wang@kiwigame.com>
2026-06-22 14:27:37 +0800

    save

--- python_client/atom/grpcAtom/base_grpc_atom.py
+++ python_client/atom/grpcAtom/base_grpc_atom.py
@@ -4,15 +4,21 @@ class BaseGrpcAtom:
 
     @classmethod
     def grpc(cls, toolkits, json_req):
+        
         grpccall = toolkits['grpc']
         module_suffix = cls.__module__.split('atom.grpcAtom.', 1)[-1]
         module_parts = module_suffix.split('.')
-        packageName = '.'.join(module_parts[:-1])
         serviceName = cls.__name__.split('GrpcAtom')[0]
+        # 兼容grpcAtom下的子包，拼接serviceName
+        packageName = '.'.join(module_parts[:-1])
         if packageName:
             serviceName = f"{packageName}.{serviceName}"
-        methodName = inspect.currentframe().f_back.f_code.co_name
+            
+        frame = inspect.currentframe()
+        caller = frame.f_back if frame is not None else None
+        methodName = caller.f_code.co_name if caller is not None else ''
+        
         url_key = serviceName.split('.')[-1]
-        print(url_key, serviceName, methodName, json_req)
+        
         response = grpccall(url_key, serviceName, methodName, json_req)
         return response
