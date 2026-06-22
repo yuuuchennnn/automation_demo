29bcefd338c0479448ffa00781d727ae95b35467
yuchen.wang <yuchen.wang@kiwigame.com>
2026-06-22 23:10:41 +0800

    update ci

--- python_client/tests/test_greeter.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from reflection_client import say_hello_with_reflection
-
-
-def test_say_hello_returns_name_through_reflection() -> None:
-    addr = os.getenv("GRPC_ADDR", "192.168.31.46:50051")
-
-    assert say_hello_with_reflection(addr, "Pytest") == "Hello, Pytest!"
